6bf45ace89fac5e1c80f4bd82b320360ff2c47db
alexander-1092 <Alex-Sadr1092@yandex.ru>
2024-09-21 14:59:41 +0300

    feat: remove popup + set name user

--- random-game/js/mapNewGame.js
+++ random-game/js/mapNewGame.js
@@ -1,6 +1,22 @@
-let game = {
-  nameUser: ["Alex", "Boris"],
-  record: [{ name: "Alex" }],
+const popupNewGameBtn = document.querySelector(".popup-NewGame__btn");
+const popupNewGameNameUser = document.querySelector(".popup-NewGame__nameUser");
+const wrapper = document.querySelector(".wrapper");
+const popupNewGame = document.querySelector(".popup-NewGame");
+const userName = {};
+
+popupNewGameBtn.addEventListener("click", () => {
+  removePopup(popupNewGameNameUser);
+});
+
+const removePopup = (popupNewGameNameUser) => {
+  if (popupNewGameNameUser.value.length !== 0) {
+    wrapper.classList.add("activ-wrapper");
+    popupNewGame.classList.add("inactive-popup-NewGame");
+    setNameUser(popupNewGameNameUser.value);
+    localStorage.setItem(userName.userName, JSON.stringify(userName));
+  }
 };
 
-localStorage.setItem("NameUser", JSON.stringify(game));
+const setNameUser = (name) => {
+  userName.userName = name;
+};
