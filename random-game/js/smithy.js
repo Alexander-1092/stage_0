f0c33d96be0e62f710bab2a1cc02c6ad7f8af877
alexander-1092 <Alex-Sadr1092@yandex.ru>
2024-09-24 19:38:51 +0300

    feat: add smithy page + moved base func

---
+++ random-game/js/smithy.js
@@ -0,0 +1,73 @@
+const userName = JSON.parse(localStorage.getItem("userName")).userName;
+
+//функция для перезаписи скиллов
+const addSkillLocalStorage = () => {
+  let data = JSON.parse(localStorage.getItem("skills"));
+  data.push("doungeon");
+  localStorage.setItem("skills", JSON.stringify(data));
+};
+
+const gameItemsIconSkills = document.querySelectorAll(
+  ".gameItems__icon_skills"
+);
+
+const showReceivedSkills = () => {
+  let dataSkills = JSON.parse(localStorage.getItem("skills"));
+
+  dataSkills.forEach((skill) => {
+    if (skill === "noob") {
+      gameItemsIconSkills[0].classList.add("active-gameItems__icon_skills");
+    } else if (skill === "doungeon") {
+      gameItemsIconSkills[1].classList.add("active-gameItems__icon_skills");
+    } else if (skill === "smithy") {
+      gameItemsIconSkills[2].classList.add("active-gameItems__icon_skills");
+    } else if (skill === "labyrinth") {
+      gameItemsIconSkills[3].classList.add("active-gameItems__icon_skills");
+    } else if (skill === "shadow") {
+      gameItemsIconSkills[4].classList.add("active-gameItems__icon_skills");
+    } else if (skill === "sky") {
+      gameItemsIconSkills[5].classList.add("active-gameItems__icon_skills");
+    }
+  });
+};
+
+showReceivedSkills();
+
+//
+
+// Блок сердечек
+const delheart = () => {
+  let counterHeart = localStorage.getItem("counterHeart");
+  restarHeart(counterHeart);
+  localStorage.setItem("counterHeart", JSON.stringify(counterHeart - 1));
+  if (counterHeart == 1) {
+    stopPlaySoundMain();
+    showYouLoose(userName);
+    playSoundLoose();
+  }
+  checkHeart(counterHeart - 1);
+};
+
+const gameItemsBoxHeart = document.querySelector(".gameItems__box-heart");
+
+const checkHeart = () => {
+  let counterHeart = localStorage.getItem("counterHeart");
+  createHeart(Number(counterHeart));
+};
+
+const createHeart = (counterHeart) => {
+  for (let index = 0; index < counterHeart; index++) {
+    gameItemsBoxHeart.insertAdjacentHTML(
+      "afterbegin",
+      "<img class='gameItems__icon' src='./assets/img/general/240px-Heart.svg.png'alt='icon heart'>"
+    );
+  }
+};
+
+checkHeart();
+
+const restarHeart = (counterHeart) => {
+  for (let index = 0; index < counterHeart; index++) {
+    gameItemsBoxHeart.removeChild(gameItemsBoxHeart.children[0]);
+  }
+};
