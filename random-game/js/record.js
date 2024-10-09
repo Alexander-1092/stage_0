4ddb9571f0be75d41a9ef278dd93b3d5a616d73a
alexander-1092 <Alex-Sadr1092@yandex.ru>
2024-10-09 15:52:31 +0300

    feat: add show list record

---
+++ random-game/js/record.js
@@ -0,0 +1,24 @@
+const recordList = JSON.parse(localStorage.getItem("record"));
+
+const tableNameHeroes = document.querySelectorAll(".table__name-heroes");
+const tableNameHeart = document.querySelectorAll(".table__name-heart");
+
+const checkLenghtRecordList = (recordList) => {
+  if (recordList.length <= 10) {
+    showTablRecord(recordList);
+  } else {
+    showTablRecord(recordList.slice(0, 10));
+  }
+};
+
+const showTablRecord = (recordList) => {
+  for (let index = 0; index < recordList.length; index++) {
+    tableNameHeroes[index].textContent = recordList[index][0];
+    tableNameHeart[index].textContent = recordList[index][1];
+  }
+};
+
+if (recordList) {
+  recordList.sort((a, b) => b[1] - a[1]);
+  checkLenghtRecordList(recordList);
+}
