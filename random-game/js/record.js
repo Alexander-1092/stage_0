4a2f2ebb6752788d81eabf349c489003c2204728
alexander-1092 <Alex-Sadr1092@yandex.ru>
2024-10-14 17:15:35 +0300

    fix: remove console.log

--- random-game/js/record.js
+++ random-game/js/record.js
@@ -23,7 +23,6 @@ const showTablRecord = (recordList) => {
 if (recordList) {
   recordList.sort((a, b) => {
     if (a[1] > 0) {
-      console.log(a[1]);
       return b[1] - a[1];
     } else {
       return b[2] - a[2];
